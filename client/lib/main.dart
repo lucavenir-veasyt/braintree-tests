1e035da310c5eda19b537253de7c67e91c44955d
~venir <luca.venir@veasyt.com>
2026-04-16 12:34:08 +0200

    main re-format

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,12 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -12,7 +17,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text("Hello World!"),
         ),
       ),
     );
